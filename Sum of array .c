165043f62ba4c39a745a686959e7958c8bcf53cb
shahanazhussainpl-cell <shahanazhussainpl@gmail.com>
2025-11-19 11:49:20 +0530

    Add program to sum elements of an array
    Implement a program to calculate the sum of an array.

---
+++ Sum of array .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n]; 
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {
+        sum += arr[i];
+    }
