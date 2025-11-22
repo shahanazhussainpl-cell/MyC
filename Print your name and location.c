ececdd380dfdce795c03e846e747645c675e9562
shahanazhussainpl-cell <shahanazhussainpl@gmail.com>
2025-11-22 11:51:00 +0530

    Create Print your name and location.c

---
+++ Print your name and location.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "John Doe";
+    int age = 25;
+    char location[] = "New York, USA";
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Location: %s\n", location);
+
+    return 0;
+}
