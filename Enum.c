3e0505315778934bd8d476e29c09428804d99ec3
shahanazhussainpl-cell <shahanazhussainpl@gmail.com>
2025-11-22 10:38:35 +0530

    Create Enum.c

---
+++ Enum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+enum week {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+int main () {
+    enum week today;
+    today=Monday;
+    printf("%d",today);
+}
+
