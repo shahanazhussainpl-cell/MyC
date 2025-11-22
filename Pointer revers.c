a08412e1d89f0f054d27562e54249762cf0a56c0
shahanazhussainpl-cell <shahanazhussainpl@gmail.com>
2025-11-22 11:47:03 +0530

    Create Pointer revers.c

---
+++ Pointer revers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void reverseNumber(int *n, int *rev) {
+    int temp = *n;
+    *rev = 0;
+    while (temp != 0) {
+        *rev = (*rev * 10) + (temp % 10);
+        temp = temp / 10;
+    }
+}
+int main() {
+    int num, reverse = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    reverseNumber(&num, &reverse);
+    printf("Reversed Number = %d\n", reverse);
+}
